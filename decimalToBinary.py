5831a68ec21a26b1ac4eea146563ceed9b043ffc
Sankeerth Ajith <sankeerthajith3@gmail.com>
2021-12-03 19:50:08 +0530

    Decimal to Binary

---
+++ decimalToBinary.py
@@ -0,0 +1,15 @@
+number = int(input("Give Number: "))
+nm = number
+fromBase = int(input("Give From base: "))
+toBase = int(input("Give To base: "))
+emp = ""
+
+if fromBase == 10 and toBase == 2:
+    while(number):
+        rem = number % 2
+        number = int(number/2)
+        emp += str(rem)
+else:
+    print("Invalid input !!")
+
+print("{} is Binary conversion of {}".format(emp, nm))
